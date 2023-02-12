5e38b4ceb48732c35c5ac92dcbe356cde6303549
Matthew Hill <matthill265@gmail.com>
2023-02-11 17:08:12 -0700

    Got all my outlets connected and most of my helper functions working on my DetailVC. Still need to work on the Delete button

--- SportsTeams.mchill/View Controllers/TeamDetailViewController.swift
+++ SportsTeams.mchill/View Controllers/TeamDetailViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class TeamDetailViewController: UIViewController {
+    // MARK: - Outlets
 
+    @IBOutlet weak var teamNameTextField: UITextField!
+    @IBOutlet weak var teamRankTextField: UITextField!
+    @IBOutlet weak var playerCountTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateView()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var team: Team?
+   // MARK: - Actions
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        clearView()
     }
-    */
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    func updateView() {
+        guard let team = team else {return}
+        teamNameTextField.text = team.teamName
+        teamNameTextField.text = "\(team.teamRank)"
+        playerCountTextField.text = "\(team.playercount)"
+    }
+    func clearView() {
+        teamNameTextField.text = ""
+        teamRankTextField.text = ""
+        playerCountTextField.text = ""
+    }
+
 
 }
